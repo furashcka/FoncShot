e2d161feeb23832b0b46c045bdd4388c753a799d
furashcka <furashcka@gmail.com>
2021-11-08 00:19:11 +0300

    render screenshots

--- src/browser/app.js
+++ src/browser/app.js
@@ -1,5 +1,16 @@
+const _ = require("lodash");
 const { ipcRenderer } = require("electron");
+const canvasElement = document.querySelector("#canvas");
+const ctx = canvasElement.getContext("2d");
 
-ipcRenderer.on("rerender", function (event, { screenshots }) {
-  console.log("screenshots", screenshots);
+ipcRenderer.on("rerender", function (e, { screenshots }) {
+  _.each(screenshots, ({ base64 }, i) => {
+    let img = new Image();
+
+    img.onload = () => {
+      let offset = _.get(screenshots, `[${i - 1}].width`, 0);
+      ctx.drawImage(img, offset, 0);
+    };
+    img.src = base64;
+  });
 });
--- src/getScreenshots.js
+++ src/getScreenshots.js
@@ -1,5 +1,7 @@
 const _ = require("lodash");
+const fs = require("fs");
 const screenshot = require("screenshot-desktop");
+const { nativeImage } = require("electron");
 
 module.exports = async function getScreenshots() {
   let displays = await screenshot.listDisplays();
@@ -8,9 +10,15 @@ module.exports = async function getScreenshots() {
   });
   let screenshots = await Promise.all(promises);
 
-  return _.map(displays, ({ width, height }, i) => ({
-    width,
-    height,
-    data: screenshots[i],
-  }));
+  return _.map(displays, ({ width, height }, i) => {
+    let base64 =
+      "data:image/jpeg;base64," +
+      Buffer.from(screenshots[i]).toString("base64");
+
+    return {
+      width,
+      height,
+      base64,
+    };
+  });
 };
