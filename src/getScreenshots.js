d07abb30a8ef508c956d73b231f0cf4b2a7da76d
furashcka <furashcka@gmail.com>
2021-11-08 00:21:13 +0300

    jpeg to png

--- src/getScreenshots.js
+++ src/getScreenshots.js
@@ -6,14 +6,13 @@ const { nativeImage } = require("electron");
 module.exports = async function getScreenshots() {
   let displays = await screenshot.listDisplays();
   let promises = _.map(displays, ({ id }) => {
-    return screenshot({ screen: id, format: "jpeg" });
+    return screenshot({ screen: id, format: "png" });
   });
   let screenshots = await Promise.all(promises);
 
   return _.map(displays, ({ width, height }, i) => {
     let base64 =
-      "data:image/jpeg;base64," +
-      Buffer.from(screenshots[i]).toString("base64");
+      "data:image/png;base64," + Buffer.from(screenshots[i]).toString("base64");
 
     return {
       width,
